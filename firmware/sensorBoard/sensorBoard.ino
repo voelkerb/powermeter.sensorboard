2ba9bb7d590950f7954efca8903f13d005657614
Benjamin Völker <voelkerb@me.com>
2021-08-18 14:13:07 +0200

    added support for double press, long press

--- firmware/sensorBoard/sensorBoard.ino
+++ firmware/sensorBoard/sensorBoard.ino
@@ -12,17 +12,34 @@
 #include <SoftwareSerial.h>
 #include <EEPROM.h>
 
+// Choose one of the following
+// Either you get press and release 
+// #define STATEFULL_PRESS
+// Or you get Single press, double press, long press etc.
+
+
+#if defined(ESP8266)
+void ICACHE_RAM_ATTR buttonISR();
+#endif
+enum class BUTTON_PRESS {NONE=0, SINGLE=1, DOUBLE=2, LONG_START=3, LONG_RELEASE=4};
+void buttonPressed(BUTTON_PRESS press);
 // pins
 #define LED_PIN 5
 #define PIR_PIN 7
 #define DHT_PIN 9
+// TODO
+#if defined(ESP8266)
+#define BUTTON_PIN D6
+#else 
 #define BUTTON_PIN 3
+#endif
 
 #define BRIGHTNESS_ADDRESS 0
 
 // Serial stuff
 #define SERIAL_SPEED 38400
-//# define DEBUG
+// #define DEBUG
+#define USE_SENSORS
 #define debugSerial Serial
 #define com Serial
 
@@ -35,8 +52,9 @@ CRGB black = CRGB::Black;
 bool ledUpdate = false;
 
 //DHT
+#ifdef USE_SENSORS
 DHT dht(DHT_PIN, DHT22);
-
+#endif
 // automaticall send data on change and with hysteresis
 bool autoSend = false;
 
@@ -58,9 +76,142 @@ long humUpdate = millis();
 
 uint8_t fadeDelay = 10;
 
+#define DOUBLE_PRESS_DELAY 900
+#define LONG_PRESS_DELAY 5000
+// Button pressed flag handled in loop
+bool buttonReleased = false;
+volatile int pressed = 0;
+volatile long lastPressed = millis();
+
+volatile bool oldState = false;
+volatile long lastTime = millis();
+
+#ifdef STATEFULL_PRESS
+void handleButton() {
+  if (pressed != 0) {
+    BUTTON_PRESS buttonPress = BUTTON_PRESS::NONE;
+    // Button was already long pressed and is now released
+    if (pressed == -1 and buttonReleased) {
+      buttonPress = BUTTON_PRESS::LONG_RELEASE;
+      pressed = 0;
+    // Button has been pressed
+    } else if (pressed > 0) {
+      // Button has been released and doublepress detection time has passed
+      if (buttonReleased and millis()-lastPressed > DOUBLE_PRESS_DELAY) {
+        // number stored in pressed corresponds to the number of presses
+        if (pressed == 1) {
+          buttonPress = BUTTON_PRESS::SINGLE;
+        } else if (pressed == 2) {
+          buttonPress = BUTTON_PRESS::DOUBLE;
+        } else {
+          // Treet as single press
+          buttonPress = BUTTON_PRESS::SINGLE;
+        }
+        // Reset variable
+        pressed = 0;
+      // Button not yet released but long press delay already passed
+      } else if (!buttonReleased and millis()-lastPressed > LONG_PRESS_DELAY) {
+        // If long press start not indicated yet
+        if (pressed != -1) {
+          buttonPress = BUTTON_PRESS::LONG_START;
+          pressed = -1;
+        } 
+      }  
+    }
+    if (buttonPress != BUTTON_PRESS::NONE) buttonPressed(buttonPress);
+  }
+}
+
+void buttonPressed(BUTTON_PRESS press) {
+  switch (press) {
+    case BUTTON_PRESS::NONE:
+      break;
+    case BUTTON_PRESS::SINGLE:
+      #ifdef DEBUG
+      debugSerial.println("Single press");
+      #endif
+      com.println("!b1");
+      break;
+    case BUTTON_PRESS::DOUBLE:
+      #ifdef DEBUG  
+      debugSerial.println("Double press");
+      #endif
+      com.println("!b2");
+      break;
+    case BUTTON_PRESS::LONG_START:
+      #ifdef DEBUG
+      debugSerial.println("Long press start");
+      #endif
+      com.println("!b3");
+      break;
+    case BUTTON_PRESS::LONG_RELEASE:
+      #ifdef DEBUG
+      debugSerial.println("Long press end");
+      #endif
+      com.println("!b4");
+      break;
+    default:
+      break;
+  }
+}
+
+#if defined(ESP8266)
+void ICACHE_RAM_ATTR buttonISR() {
+#else
+void buttonISR() {
+#endif
+  bool state = !digitalRead(BUTTON_PIN);
+  if (state == oldState) return;
+  // Debounce delay
+  if (millis()-lastTime < 10) return;
+  lastTime = millis();
+  if (state) {
+    #ifdef DEBUG
+    debugSerial.println("Pressed");
+    #endif
+    pressed++;
+    lastPressed = millis();
+    buttonReleased = false;
+  } else {
+    #ifdef DEBUG
+    debugSerial.println("Released");
+    #endif
+    buttonReleased = true;
+  }
+  oldState = state;
+}
+
+#else 
+#if defined(ESP8266)
+void ICACHE_RAM_ATTR buttonISR() {
+#else
+void buttonISR() {
+#endif
+  bool state = !digitalRead(BUTTON_PIN);
+  if (state == oldState) return;
+  // Debounce delay
+  if (millis()-lastTime < 10) return;
+  lastTime = millis();
+  if (state) {
+    #ifdef DEBUG
+    debugSerial.println("Pressed");
+    #endif
+    com.println("!b");
+  } else {
+    #ifdef DEBUG
+    debugSerial.println("Released");
+    #endif
+    com.println("!r");
+  }
+  oldState = state;
+}
+#endif
+
 void setup() {
   #ifdef DEBUG
+  #if debugSerial != com
   debugSerial.begin(SERIAL_SPEED);
+  #endif
   #endif
   // Init serial
   com.begin(SERIAL_SPEED);
@@ -81,17 +232,21 @@ void setup() {
   allLEDs(fadeColor, NUM_LEDS, black);
   // Fade black
   ledUpdate = true;
+  #if defined(ESP8266)
+  EEPROM.begin(512);
+  #endif
   // Load and set brightness
   EEPROM.get(BRIGHTNESS_ADDRESS, brightness);
   FastLED.setBrightness(brightness);
-
   // Set button as input and configure pin interrupt
-  pinMode(BUTTON_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, FALLING); 
-
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonISR, CHANGE); 
+  #ifdef USE_SENSORS
   // PIR has digital input signal
-  pinMode(PIR_PIN, INPUT);
-
+  #ifdef DEBUG
+  debugSerial.println("PIR init");
+  pinMode(PIR_PIN, INPUT_PULLUP);
+  #endif
   // Wire required for TSL
   Wire.begin();
   #ifdef DEBUG
@@ -102,38 +257,20 @@ void setup() {
   debugSerial.println("TSL2561 init");
   #endif
   TSL2561.init();
+  #endif
+
   #ifdef DEBUG
   debugSerial.println("Setup done!");
   #endif
 }
 
-// Debounce timer
-long buttonDebounce = millis();
-// Flag handled in loop
-bool buttonPressed = false;
-void buttonISR() {
-  if (millis()-buttonDebounce > 300) {
-    // buttonPressed = true;
-    buttonDebounce = millis();
-    // // If serial communication was interrupted
-    // com.flush();
-    // delay(1);
-    // com.println("!b");
-    // delay(1);
-    buttonPressed = true;
-  }
-}
+
 
 void loop() {
-  // NOTE: Sensor reading might take much time
-  // if (buttonPressed) {
-  //   #ifdef DEBUG
-  //   debugSerial.println("Button Pressed");
-  //   #endif
-  //   com.println("!b");
-  //   buttonPressed = false;
-  // }
-
+  #ifdef STATEFULL_PRESS
+  handleButton();
+  #endif
+  #ifdef USE_SENSORS
   if (autoSend) {
     sendPIR(true);
     if (millis()-lightUpdate > LIGHT_UPDATE_INTV) {
@@ -149,6 +286,7 @@ void loop() {
       sendTemp(true);
     }
   }
+  #endif
   // As long as fade is not finished, continue fading
   if (ledUpdate) {
     ledUpdate = !fadeit();
@@ -162,10 +300,12 @@ void loop() {
 
 
 void handleEvent() {
-  if (!com.available()) return;
+  if (!(com.available()>2)) return;
   char c = com.read();
   // If it is not a !, return
-  if (c != '?' and c != '!') return avail;
+  if (c != '?' and c != '!') return;
+  // Let all data be sent by the sink
+  delay(10);
   char d = com.read();
   switch (d) {
     case '?':
@@ -176,6 +316,11 @@ void handleEvent() {
       EEPROM.put(BRIGHTNESS_ADDRESS, brightness);
       FastLED.setBrightness(brightness);
       FastLED.show();
+      #ifdef DEBUG
+      debugSerial.print("Set brightess to: ");
+      EEPROM.get(BRIGHTNESS_ADDRESS, brightness);
+      debugSerial.println(brightness);
+      #endif
       break;
     case 'a':
       autoSend = true;
@@ -187,9 +332,17 @@ void handleEvent() {
       hum = -1;
       light = -1;
       pir = 255;
+      #ifdef DEBUG
+      debugSerial.println("Autosend on");
+      #endif
+      break;
     case 'o':
       autoSend = false;
+      #ifdef DEBUG
+      debugSerial.println("Autosend off");
+      #endif
       break;
+    #ifdef USE_SENSORS
     case 'p':
       sendPIR(false);
       break;
@@ -202,6 +355,7 @@ void handleEvent() {
     case 'l':
       sendHum(false);
       break;
+    #endif
     case 'L':
       delay(10);
       if (com.available() < 3*NUM_LEDS) return;
@@ -212,6 +366,19 @@ void handleEvent() {
         fadeColor[i] = CRGB(r,g,b);
       }
       ledUpdate = true;
+      #ifdef DEBUG
+      debugSerial.print("Set LED to: ");
+      for (int i = 0; i < NUM_LEDS; i++) {
+        debugSerial.print("[");
+        debugSerial.print(fadeColor[i].red);
+        debugSerial.print(",");
+        debugSerial.print(fadeColor[i].green);
+        debugSerial.print(",");
+        debugSerial.print(fadeColor[i].blue);
+        debugSerial.print("] ");
+      }
+      debugSerial.println();
+      #endif
       break;
     default:
       #ifdef DEBUG
@@ -221,6 +388,7 @@ void handleEvent() {
   }
 }
 
+#ifdef USE_SENSORS
 void sendPIR(bool onNew) {
   uint8_t newPIR = (uint8_t)digitalRead(PIR_PIN);
   if (onNew and newPIR == pir) return;
@@ -269,6 +437,7 @@ void sendLight(bool onNew) {
   com.write(b,4);
   com.println();
 }
+#endif
 
 void allLEDs(CRGB *colors, uint32_t N, CRGB color) {
   for (int i = 0; i < N; i++) colors[i] = color;
@@ -278,8 +447,6 @@ void setAllLEDs(CRGB color) {
   allLEDs(leds, NUM_LEDS, color);
   FastLED.show();
 }
-
-
 
 bool fadeit() {
   for( uint16_t i = 0; i < NUM_LEDS; i++) {
